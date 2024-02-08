2d9a5247e88b1fa707cd75fd738c561382e1d8ff
Sean ORourke <coulpaul98@gmail.com>
2024-02-07 21:41:18 -0500

    realsence_websocket_client now uses MORB_SLAM, update CMake

--- Examples/Stereo-Inertial/realsense_websocket_client.cpp
+++ Examples/Stereo-Inertial/realsense_websocket_client.cpp
@@ -2,9 +2,13 @@
 #include <ixwebsocket/IXWebSocket.h>
 #include <ixwebsocket/IXUserAgent.h>
 #include <iostream>
+#include <condition_variable>
 #include <opencv2/opencv.hpp>
 
-int main() {
+#include <MORB_SLAM/System.h>
+#include <MORB_SLAM/Viewer.h>
+
+int main(int argc, char **argv) {
     ix::initNetSystem();
     ix::WebSocket webSocket;
 
@@ -15,27 +19,48 @@ int main() {
 
     cv::Mat left_img(cv::Size(848, 480), CV_8UC1);
     cv::Mat right_img(cv::Size(848, 480), CV_8UC1);
-    uint64_t img_timestamp = 0;
-    int image_size = left_img.total()*left_img.elemSize();
-    int timestamp_size = sizeof(uint64_t);
 
-    double accel[3];
-    double gyro[3];
-    uint64_t imu_timestamp = 0;
-    int imu_size = sizeof(double)*3;
+    // TODO make img/imu timestamps std::chrono so you can read in either sec or ms
+    // For now it's just seconds
+    double img_timestamp = 0;
+    int image_size = left_img.total()*left_img.elemSize();
+    int timestamp_size = sizeof(double);
+
+    std::vector<float> imu(6);
+    double imu_timestamp = 0;
+    int imu_size = sizeof(float)*6;
+
+    std::vector<std::vector<float>> imu_measurements;
+    std::vector<double> imu_timestamps;
+
+    std::vector<MORB_SLAM::IMU::Point> imu_points;
 
     bool connected = false;
+    bool new_img = false;
 
-    webSocket.setOnMessageCallback([&webSocket, &connected, &img_timestamp, &left_img, &right_img, &imu_timestamp, &accel, &gyro, timestamp_size, image_size, imu_size](const ix::WebSocketMessagePtr& msg) {
+    std::mutex imu_mutex;
+    std::condition_variable cond_image_rec;
+
+    
+
+    webSocket.setOnMessageCallback([&webSocket, &connected, &img_timestamp, &left_img, &right_img, &imu_timestamp, &imu_timestamps, &imu, &imu_measurements, timestamp_size, image_size, imu_size, &imu_mutex, &cond_image_rec, &new_img](const ix::WebSocketMessagePtr& msg) {
             if(msg->type == ix::WebSocketMessageType::Message) {
+                std::unique_lock<std::mutex> lock(imu_mutex);
+
                 if(msg->str.data()[0] == 1) {
                     std::memcpy(&img_timestamp, msg->str.data()+1, timestamp_size);
                     std::memcpy((char *)left_img.data, msg->str.data()+1+timestamp_size, image_size);
                     std::memcpy((char *)right_img.data, msg->str.data()+1+image_size+timestamp_size, image_size);
+                    new_img = true;
+
+                    lock.unlock();
+                    cond_image_rec.notify_all();
                 } else if(msg->str.data()[0] == 2) {
                     std::memcpy(&imu_timestamp, msg->str.data()+1, timestamp_size);
-                    std::memcpy((char *)accel, msg->str.data()+1+timestamp_size, imu_size);
-                    std::memcpy((char *)gyro, msg->str.data()+1+imu_size+timestamp_size, imu_size);
+                    std::memcpy(imu.data(), msg->str.data()+1+timestamp_size, imu_size);
+                    imu_measurements.push_back(imu);
+                    imu_timestamps.push_back(imu_timestamp);
+                    //std::memcpy((char *)gyro, msg->str.data()+1+imu_size+timestamp_size, imu_size);
                 }
             }
             else if(msg->type == ix::WebSocketMessageType::Open) {
@@ -49,21 +74,95 @@ int main() {
         }
     );
     
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    auto SLAM = std::make_shared<MORB_SLAM::System>(argv[1],argv[2], MORB_SLAM::CameraType::IMU_STEREO);
+    auto viewer = std::make_shared<MORB_SLAM::Viewer>(SLAM);
+
+    float imageScale = SLAM->GetImageScale();
+    
+    std::vector<std::vector<float>> local_imu_measurements;
+    std::vector<double> local_imu_timestamps;
+    cv::Mat local_left_img(cv::Size(848, 480), CV_8UC1);
+    cv::Mat local_right_img(cv::Size(848, 480), CV_8UC1);
+    double local_img_timestamp;
+
     webSocket.start();
 
     while(!connected) {
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    //std::this_thread::sleep_for(std::chrono::milliseconds(5000));
     while(true) {
-        cv::imshow("LEFT", left_img);
-        cv::imshow("RIGHT", right_img);
-        std::cout << accel[0] << ", " << accel[1] << ", " << accel[2] << std::endl;
-        char key = cv::waitKey(15);
-        if(key == 'q') {
-            break;
+        {
+            std::unique_lock<std::mutex> lk(imu_mutex);
+            while(!new_img)
+                cond_image_rec.wait(lk);
+
+            local_imu_measurements = imu_measurements;
+            local_imu_timestamps = imu_timestamps;
+            local_img_timestamp = img_timestamp;
+            local_left_img = left_img.clone();
+            local_right_img = right_img.clone();
+
+            imu_measurements.clear();
+            imu_timestamps.clear();
+
+            new_img = false;
         }
+
+        for(size_t i=0; i<local_imu_measurements.size(); i++) {
+            MORB_SLAM::IMU::Point new_point(local_imu_measurements[i][0], local_imu_measurements[i][1], local_imu_measurements[i][2], local_imu_measurements[i][3], local_imu_measurements[i][4], local_imu_measurements[i][5], local_imu_timestamps[i]);
+            imu_points.push_back(new_point);
+        }
+
+        // cv::imshow("LEFT", local_left_img);
+        // cv::imshow("RIGHT", local_right_img);
+        // std::cout << size(imu_points) << std::endl;
+        // if(size(imu_points) > 0) {
+        //     std::cout << imu_points[size(imu_points)-1].w[0] << ", " << imu_points[size(imu_points)-1].w[1] << ", " << imu_points[size(imu_points)-1].w[2] << std::endl;
+        //     std::cout << std::fixed;
+        //     std::cout << imu_points[0].t << std::endl;
+        // }
+        // std::cout << std::fixed;
+        // std::cout << local_img_timestamp << std::endl;
+        // char key = cv::waitKey(1);
+        // if(key == 'q') {
+        //     webSocket.close();
+        //     break;
+        // }
+
+
+        if(imageScale != 1.f) {
+            int width = local_left_img.cols * imageScale;
+            int height = local_left_img.rows * imageScale;
+            cv::resize(local_left_img, local_left_img, cv::Size(width, height));
+            cv::resize(local_right_img, local_right_img, cv::Size(width, height));
+        }
+
+
+        MORB_SLAM::StereoPacket sophusPose = SLAM->TrackStereo(local_left_img, local_right_img, local_img_timestamp, imu_points);
+
+        if(sophusPose.pose) {
+            sophusPose.pose = sophusPose.pose->inverse();
+        }
+        
+        viewer->update(sophusPose);
+
+        imu_points.clear();
     }
+
+    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    // while(true) {
+    //     cv::imshow("LEFT", left_img);
+    //     cv::imshow("RIGHT", right_img);
+    //     std::cout << accel[0] << ", " << accel[1] << ", " << accel[2] << std::endl;
+    //     char key = cv::waitKey(15);
+    //     if(key == 'q') {
+    //         webSocket.close();
+    //         break;
+    //     }
+    // }
 
     return 0;
 }
---
+++ Examples/Stereo-Inertial/run_websocket.sh
@@ -0,0 +1,1 @@
+gdb --args ../../bin/realsense_websocket_client ../../Vocabulary/ORBvoc.txt ./RealSense_D435i.yaml 
