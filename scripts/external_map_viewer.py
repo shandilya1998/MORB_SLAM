9849c38a5ca72198e8249bc2d105e7fd80d410b4
Sean ORourke <coulpaul98@gmail.com>
2024-03-11 16:32:29 -0400

    made ExternalMapViewer python script 2D

--- scripts/external_map_viewer.py
+++ scripts/external_map_viewer.py
@@ -27,91 +27,116 @@ async def main():
 
     figSLAM = plt.figure()
     figAIV = plt.figure()
-    axSLAM = figSLAM.add_subplot(111, projection=Axes3D.name)
-    axAIV = figAIV.add_subplot(111, projection=Axes3D.name)
-    axSLAM.view_init(90, -90)
-    axAIV.view_init(90, -90)
-    positionSLAM = np.zeros((0, 3))
-    positionAIV = np.zeros((0, 3))
+    # axSLAM = figSLAM.add_subplot(111, projection=Axes3D.name)
+    # axAIV = figAIV.add_subplot(111, projection=Axes3D.name)
+    axSLAM = figSLAM.add_subplot(111)
+    axAIV = figAIV.add_subplot(111)
+    # axSLAM.view_init(90, -90)
+    # axAIV.view_init(90, -90)
+    positionSLAM = np.zeros((0, 2))
+    positionAIV = np.zeros((0, 2))
 
-    scSLAM = axSLAM.scatter(positionSLAM[:, 0], positionSLAM[:, 1], positionSLAM[:, 2], s=5, c='b')
-    scAIV = axAIV.scatter(positionAIV[:, 0], positionAIV[:, 1], positionAIV[:, 2], s=5, c='r')
+    # scSLAM = axSLAM.scatter(positionSLAM[:, 0], positionSLAM[:, 1], positionSLAM[:, 2], s=5, c='b')
+    # scAIV = axAIV.scatter(positionAIV[:, 0], positionAIV[:, 1], positionAIV[:, 2], s=5, c='r')
+
+    # scSLAM = axSLAM.scatter(positionSLAM[:, 0], positionSLAM[:, 1], s=5, c='b')
+    # scAIV = axAIV.scatter(positionAIV[:, 0], positionAIV[:, 1], s=5, c='r')
 
     axSLAM.set_xlabel('X')
     axSLAM.set_ylabel('Y')
-    axSLAM.set_zlabel('Z')
+    # axSLAM.set_zlabel('Z')
     axSLAM.set_title('SLAM')
 
     axAIV.set_xlabel('X')
     axAIV.set_ylabel('Y')
-    axAIV.set_zlabel('Z')
+    # axAIV.set_zlabel('Z')
     axAIV.set_title('AIV')
    
     plt.ion()
     plt.show()
+    # mins = [-1, -1, -1]
+    # maxs = [1, 1, 1]
 
-    mins = [-1, -1, -1]
-    maxs = [1, 1, 1]
+    # axSLAM.set_xlim(mins[0], maxs[0])
+    # axSLAM.set_ylim(mins[1], maxs[1])
+    # axSLAM.set_zlim(mins[2], maxs[2])
 
-    axSLAM.set_xlim(mins[0], maxs[0])
-    axSLAM.set_ylim(mins[1], maxs[1])
-    axSLAM.set_zlim(mins[2], maxs[2])
+    # axAIV.set_xlim(mins[0], maxs[0])
+    # axAIV.set_ylim(mins[1], maxs[1])
+    # axAIV.set_zlim(mins[2], maxs[2])
 
-    axAIV.set_xlim(mins[0], maxs[0])
-    axAIV.set_ylim(mins[1], maxs[1])
-    axAIV.set_zlim(mins[2], maxs[2])
-
-    async with websockets.connect(uri, ping_interval=None) as websocket:
-        n = 0
-        while True:
-            
-            binary_data = await websocket.recv()
-            # isFromSLAM, rotation_matrix, translation_vector, state, message, isKF = parse_binary_data(binary_data)
-            isFromSLAM, translation_vector = parse_binary_data(binary_data)
-            n += 1
-            # Print received data
-            # print("Rotation Matrix:")
-            # print(rotation_matrix)
-            # print("\nTranslation Vector:")
-            # print(state)
-            # print("\nMessage:", message)
-            # print("isKF:", isKF)
-            
-            if isFromSLAM:
-                positionSLAM = np.vstack((positionSLAM, translation_vector))
-                scSLAM._offsets3d = (positionSLAM[:, 0], positionSLAM[:, 1], positionSLAM[:, 2])
-            else:
-                positionAIV = np.vstack((positionAIV, translation_vector))
-                scAIV._offsets3d = (positionAIV[:, 0], positionAIV[:, 1], positionAIV[:, 2])
-            
-            if translation_vector[0] < mins[0] or translation_vector[1] < mins[1]:
-                mins[0] *= 2
-                mins[1] *= 2
-                axSLAM.set_xlim(mins[0], maxs[0])
-                axSLAM.set_ylim(mins[1], maxs[1])
-                axAIV.set_xlim(mins[0], maxs[0])
-                axAIV.set_ylim(mins[1], maxs[1])
+    try:
+        async with websockets.connect(uri, ping_interval=None) as websocket:
+            n = 0
+            while True:
                 
-            elif translation_vector[0] > maxs[0] or translation_vector[1] > maxs[1]:
-                maxs[0] *= 2
-                maxs[1] *= 2
-                axSLAM.set_xlim(mins[0], maxs[0])
-                axSLAM.set_ylim(mins[1], maxs[1])
-                axAIV.set_xlim(mins[0], maxs[0])
-                axAIV.set_ylim(mins[1], maxs[1])
+                binary_data = await websocket.recv()
+                # isFromSLAM, rotation_matrix, translation_vector, state, message, isKF = parse_binary_data(binary_data)
+                isFromSLAM, translation_vector = parse_binary_data(binary_data)
+                n += 1
+                # Print received data
+                # print("Rotation Matrix:")
+                # print(rotation_matrix)
+                # print("\nTranslation Vector:")
+                # print(state)
+                # print("\nMessage:", message)
+                # print("isKF:", isKF)
+                
+                if isFromSLAM:
+                    # print("A")
+                    positionSLAM = np.vstack((positionSLAM, translation_vector[:2]))
+                    
+                    # scSLAM.set_offsets(positionSLAM[:, 0], positionSLAM[:, 1])
+                    # print("c")
+                else:
+                    # print("d")
+                    positionAIV = np.vstack((positionAIV, translation_vector[:2]))
+                    
+                    # scAIV._offsets3d = (positionAIV[:, 0], positionAIV[:, 1], positionAIV[:, 2])
+                    # scAIV.set_offsets(np.column_stack((positionAIV[:, 0], positionAIV[:, 1])))
+                
+                # if translation_vector[0] < mins[0] or translation_vector[1] < mins[1]:
+                #     mins[0] *= 2
+                #     mins[1] *= 2
+                #     axSLAM.set_xlim(m`ins[0], maxs[0])
+                #     axSLAM.set_ylim(mins[1], maxs[1])
+                #     axAIV.set_xlim(mins[0], maxs[0])
+                #     axAIV.set_ylim(mins[1], maxs[1])
+                    
+                # elif translation_vector[0] > maxs[0] or translation_vector[1] > maxs[1]:
+                #     maxs[0] *= 2
+                #     maxs[1] *= 2
+                #     axSLAM.set_xlim(mins[0], maxs[0])
+                #     axSLAM.set_ylim(mins[1], maxs[1])
+                #     axAIV.set_xlim(mins[0], maxs[0])
+                #     axAIV.set_ylim(mins[1], maxs[1])
 
-            if translation_vector[2] < mins[2]:
-                mins[2] *= 2
-                axSLAM.set_zlim(mins[2], maxs[2])
-                axAIV.set_zlim(mins[2], maxs[2])
-            elif translation_vector[2] > maxs[2]:
-                maxs[2] *= 2
-                axSLAM.set_zlim(mins[2], maxs[2])
-                axAIV.set_zlim(mins[2], maxs[2])
+                # if translation_vector[2] < mins[2]:
+                #     mins[2] *= 2
+                #     axSLAM.set_zlim(mins[2], maxs[2])
+                #     axAIV.set_zlim(mins[2], maxs[2])
+                # elif translation_vector[2] > maxs[2]:
+                #     maxs[2] *= 2
+                #     axSLAM.set_zlim(mins[2], maxs[2])
+                #     axAIV.set_zlim(mins[2], maxs[2])
 
-            if n == 8:
-                plt.pause(0.01)
-                plt.draw()
-                n=0
+                if n == 10:
+                    axAIV.plot(positionAIV[:, 0], positionAIV[:, 1])
+                    axSLAM.plot(positionSLAM[:, 0], positionSLAM[:, 1])
+                    plt.pause(0.05)
+                    # plt.draw()
+                    n=0
+    except:
+        axSLAM.plot(positionSLAM[:, 0], positionSLAM[:, 1], 'bo', markersize=3)
+        axAIV.plot(positionAIV[:, 0], positionAIV[:, 1], 'go', markersize=3)
+        plt.pause(0.01)
+        plt.draw()
+        while True:
+            if plt.waitforbuttonpress():
+                event = plt.gcf().canvas.manager.key_press_handler
+                if event.key == 'q':
+                    plt.close()
+                    break
+
 
 asyncio.run(main())
