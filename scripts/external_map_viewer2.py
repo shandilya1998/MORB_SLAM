26e82f06e606ba4b201b56047109d01767e68ba7
Sean ORourke <coulpaul98@gmail.com>
2024-03-12 03:28:55 -0400

    updated external_map_viewer2.py

--- scripts/external_map_viewer2.py
+++ scripts/external_map_viewer2.py
@@ -3,6 +3,7 @@ import cv2
 import websockets
 import numpy as np
 import matplotlib.pyplot as plt
+from threading import Thread, Lock
 
 def parse_binary_data(binary_data):
         isFromSLAM = binary_data[0]
@@ -23,33 +24,29 @@ def parse_binary_data(binary_data):
 async def main():
     uri = "ws://192.168.1.1:9002"
 
-    p_SLAM = fancy_matplot("SLAM")
-    p_AIV = fancy_matplot("AIV")
-
     try:
         async with websockets.connect(uri, ping_interval=None) as websocket:
             n = 0
             n2 = 0
-            c = 5
-            while True:
-                
+            c = 3
+            while not shouldClose:
+                # async with asyncio.timeout(timeout=1.5):
                 binary_data = await websocket.recv()
-                
                 isFromSLAM, translation_vector = parse_binary_data(binary_data)
                 n += 1
                 n2 += 1
                 if isFromSLAM:
                     if n > c:
-                        if p_SLAM.update(translation_vector[0], translation_vector[1]):
-                            plt.pause(0.01)
+                        if p_SLAM.update(translation_vector[0], translation_vector[1], False):
                             n=0
                 else:
                     if n2 > c:
-                        if p_AIV.update(translation_vector[0], translation_vector[1]):
-                            plt.pause(0.01)
+                        if p_AIV.update(translation_vector[0], translation_vector[1], False):
                             n2=0
+                
     except:
-        plt.pause(0)
+        import traceback
+        traceback.print_exc()
         
 
 def minMax(minX, maxX, minY, maxY, padding=0.1, numTicks=10):
@@ -72,38 +69,73 @@ def minMax(minX, maxX, minY, maxY, padding=0.1, numTicks=10):
 
 class fancy_matplot:
     def __init__(self, name=None):
-        self.fig = plt.figure()
+        self.fig = plt.figure(figsize=(8,8))
         self.ax = self.fig.add_subplot(111)
         self.Ln, = self.ax.plot([0,0])
         self.datX=[]
         self.datY=[]
+        self.lock = Lock()
         if not name is None:
             self.ax.set_title(name)
         self.minX = float('inf')
         self.maxX = -float('inf')
         self.minY = float('inf')
         self.maxY = -float('inf')
-    def update(self, valX, valY):
-        if valX < self.minX: self.minX = valX
-        if valY < self.minY: self.minY = valY
-        if valX > self.maxX: self.maxX = valX
-        if valY > self.maxY: self.maxY = valY
-        # print(np.sqrt((valX-self.datX[-1])**2 + (valY-self.datY[-1])**2))
-        if len(self.datX) < 4 or len(self.datY) < 4 or (not (np.sqrt((valX-self.datX[-1])**2 + (valY-self.datY[-1])**2) < 0.0001)):
-            self.datX.append(valX)
-            self.datY.append(valY)
-            self.Ln.set_ydata(self.datY)
-            self.Ln.set_xdata(self.datX)
-            xr, yr = minMax(self.minX, self.maxX, self.minY, self.maxY)
-            self.ax.set_xticks(xr)
-            self.ax.set_yticks(yr)
-            self.fig.canvas.draw()
-            return True
-        return False
+        self.updated = False
+
+    def update(self, valX, valY, doDraw=True):
+        updated = False
+        with self.lock:
+            if valX < self.minX: self.minX = valX
+            if valY < self.minY: self.minY = valY
+            if valX > self.maxX: self.maxX = valX
+            if valY > self.maxY: self.maxY = valY
+            # print(np.sqrt((valX-self.datX[-1])**2 + (valY-self.datY[-1])**2))
+            if len(self.datX) < 4 or len(self.datY) < 4 or (not (np.sqrt((valX-self.datX[-1])**2 + (valY-self.datY[-1])**2) < 0.0001)):
+                self.datX.append(valX)
+                self.datY.append(valY)
+                self.updated = True
+                updated = True
+        if doDraw:
+            self.draw()
+        return updated
+
+    def draw(self):
+
+        with self.lock:
+
+            if self.updated:
+                
+                self.Ln.set_ydata(self.datY)
+                self.Ln.set_xdata(self.datX)
+                xr, yr = minMax(self.minX, self.maxX, self.minY, self.maxY)
+                self.ax.set_xticks(xr)
+                self.ax.set_yticks(yr)
+                self.fig.canvas.draw()
+                self.updated = False
     def getImage(self):
-        img = np.fromstring(self.fig.canvas.tostring_rgb(), dtype=np.uint8,sep='')
-        img  = img.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
-        img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+        with self.lock:
+            img = np.fromstring(self.fig.canvas.tostring_rgb(), dtype=np.uint8,sep='')
+            img  = img.reshape(self.fig.canvas.get_width_height()[::-1] + (3,))
+            img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
         return img
-      
-asyncio.run(main())
+
+p_SLAM = fancy_matplot("SLAM")
+p_AIV = fancy_matplot("AIV")
+shouldClose = False
+thread = Thread(target=lambda:asyncio.run(main()))
+thread.start()
+# asyncio.run(main())
+while True:
+    p_SLAM.draw()
+    p_AIV.draw()
+    if thread.is_alive():
+        plt.pause(0.1)
+    else:
+        break
+
+shouldClose = True
+plt.pause(0)
+print('waiting for websocket to join...')
+thread.join()
+print('all done :)')
